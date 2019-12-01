4ef6305450bea0a437ef7ac714cc0f24ba162673
Jenny Schweers <orange.jenny@gmail.com>
2019-12-01 17:43:02 -0500

    Added miles models and import commands

---
+++ miles/management/commands/import_days.py
@@ -0,0 +1,37 @@
+from argparse import RawTextHelpFormatter
+from datetime import date
+import json
+import re
+
+from django.core.management.base import BaseCommand, CommandError
+
+from miles.models import Day
+
+
+class Command(BaseCommand):
+    def create_parser(self, *args, **kwargs):
+        parser = super(Command, self).create_parser(*args, **kwargs)
+        parser.formatter_class = RawTextHelpFormatter
+        return parser
+
+    @property
+    def help(self):
+        return "Import days from a json file. Query: select day, notes from days"
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', help="JSON file of data => [item, item...]")
+        parser.add_argument('--save', action='store_true')
+
+    def handle(self, *args, **options):
+        filename = options['filename']
+        save = options['save']
+
+        with open(filename, encoding='utf-8') as f:
+            data = json.load(f)['data']
+            for item in data:
+                day_parts = [int(part) for part in re.split(r'\D+', item['day'])]
+                day = Day(day=date(year=day_parts[0], month=day_parts[1], day=day_parts[2]), notes=item['notes'])
+                if save:
+                    day.save()
+            self.stdout.write("{}Imported {} days".format("" if save else "[DRY RUN] ", len(data)))
+
---
+++ miles/management/commands/import_workouts.py
@@ -0,0 +1,58 @@
+from argparse import RawTextHelpFormatter
+from datetime import date
+import json
+import re
+
+from django.core.management.base import BaseCommand, CommandError
+
+from miles.models import Day Workout
+
+
+class Command(BaseCommand):
+    def create_parser(self, *args, **kwargs):
+        parser = super(Command, self).create_parser(*args, **kwargs)
+        parser.formatter_class = RawTextHelpFormatter
+        return parser
+
+    @property
+    def help(self):
+        return '''
+            Import workouts from a json file. Associated days must already exist.
+            Query: select day, activity, time, distance, sets, reps, weight, unit from days, workouts where workouts.day_id = days.id
+        '''
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', help="JSON file of data => [item, item...]")
+        parser.add_argument('--save', action='store_true')
+
+    def handle(self, *args, **options):
+        filename = options['filename']
+        save = options['save']
+
+        with open(filename, encoding='utf-8') as f:
+            data = json.load(f)['data']
+            for item in data:
+                day_parts = re.split(r'\D+', item['day'])
+                day = date(year=day_parts[0], month=day_parts[1], day=day_parts[2])
+                try:
+                    pass
+                    # TODO
+                    #day = Album.objects.get(id=item['collectionid'])
+                except Day.DoesNotExist:
+                    # TODO
+                    pass
+                workout = Workout(
+                    activity=item['activity'],
+                    time=item['time'],
+                    distance=item['distance'],
+                    distance_unit=item['unit'],
+                    sets=item['sets'],
+                    reps=item['reps'],
+                    weight=item['weight'],
+                    day=day,
+                )
+                if save:
+                    workout.save()
+                '''
+            self.stdout.write("{}Imported {} workouts".format("" if save else "[DRY RUN] ", len(data)))
+
